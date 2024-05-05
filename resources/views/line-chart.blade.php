5f555f1bc9e281b9b7f9de9992b31d9b6152dccc
CLongHome <sdavid0218@gmail.com>
2024-05-05 18:58:41 +0800

    feat:show table and max value of each row

--- resources/views/line-chart.blade.php
+++ resources/views/line-chart.blade.php
@@ -6,6 +6,28 @@
 </head>
 <body>
     <canvas id="lineChart" width="800" height="400"></canvas>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Machine</th>
+                @foreach ($labels as $label)
+                    <th>{{ $label }}</th>
+                @endforeach
+                <th>Max</th> <!-- 新添加的列 -->
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($datasets as $dataset)
+                <tr>
+                    <td>{{ $dataset['label'] }}</td>
+                    @foreach ($dataset['data'] as $data)
+                        <td>{{ $data }}</td>
+                    @endforeach
+                    <td>{{ max($dataset['data']) }}</td> <!-- 显示每一行数据的最大值 -->
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
     <script>
         var labels = {!! json_encode($labels) !!};
         var datasets = {!! json_encode($datasets) !!};
@@ -18,7 +40,7 @@
                 datasets: datasets
             },
             options: {
-                responsive: true,
+                responsive: false,
                 maintainAspectRatio: false,
             }
         });
