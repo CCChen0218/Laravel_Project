978b543207bd85a7a3c5e5d1661876c4c6920838
CLongHome <sdavid0218@gmail.com>
2024-05-05 19:25:42 +0800

    feat:show low-yield warning

--- resources/views/line-chart.blade.php
+++ resources/views/line-chart.blade.php
@@ -2,6 +2,22 @@
 <html>
 <head>
     <title>Line Chart</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 50%;
+        }
+        th, td {
+            text-align: center;
+            padding: 8px;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+        .warning {
+            background-color: #ffcccc;
+        }
+    </style>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body>
@@ -13,17 +29,19 @@
                 @foreach ($labels as $label)
                     <th>{{ $label }}</th>
                 @endforeach
-                <th>Max</th> <!-- 新添加的列 -->
+                <th>low-yield-warning</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($datasets as $dataset)
                 <tr>
                     <td>{{ $dataset['label'] }}</td>
-                    @foreach ($dataset['data'] as $data)
+                    @foreach ($dataset['data'] as $key => $data)
                         <td>{{ $data }}</td>
                     @endforeach
-                    <td>{{ max($dataset['data']) }}</td> <!-- 显示每一行数据的最大值 -->
+                    <td class="{{ $dataset['data'][count($dataset['data']) - 1] < array_sum(array_slice($dataset['data'], 0, -1)) / (count($dataset['data']) - 1) ? 'warning' : '' }}">
+                        {{ $dataset['data'][count($dataset['data']) - 1] < array_sum(array_slice($dataset['data'], 0, -1)) / (count($dataset['data']) - 1) ? 'Warning' : '' }}
+                    </td>
                 </tr>
             @endforeach
         </tbody>
@@ -47,3 +65,4 @@
     </script>
 </body>
 </html>
+
